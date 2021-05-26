b951ae79876adc137226e0f3a4791195fa6029f9
Elena <e.cotar327@gmail.com>
2021-05-26 18:59:50 +0300

    Fixed the function so it iterates on the service level values

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
 //(totalbillamount*servicelevel)/numberof people
 const billAmount_input = document.querySelector('.bill');
-const serviceLevel_input = document.querySelector('.servLevel');
+const serviceLevel_input = document.querySelectorAll('.servLevel');
 const numOfPeople_input = document.querySelector('.people');
 
 const tip = () => {
     let billAmount = Number(billAmount_input.value);
-    let serviceLevel = Number(serviceLevel_input.value);
+    for (let i = 0; i < serviceLevel_input.length; i++) {
+        if (serviceLevel_input[i].checked == true) {
+            serviceLevel = Number(
+                document.querySelectorAll('.servLevel')[i].value
+            );
+        }
+    }
     let numOfPeople = Number(numOfPeople_input.value);
     const res = (billAmount * serviceLevel) / numOfPeople;
     document.getElementById(
